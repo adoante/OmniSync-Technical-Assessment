f0358ea69f55ed16d1c8105779c11ee6c2b9caf5
Adolfo Gante <adolfogante@gmail.com>
2025-09-20 13:23:59 -0700

    feat(api): POST(createCard) and GET(getCards) handlers

---
+++ app/api/cards/route.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+import { createCard, getCards } from "@/lib/db";
+import type { Card, CreateCardRequest, CardsResponse, CardResponse } from "@/types/card";
+
+export async function POST() {
+	const request: CreateCardRequest = { createdAt: new Date() }
+	const card: Card = await createCard(request)
+	const response: CardResponse = { card: card }
+	return NextResponse.json(response, { status: 200 })
+}
+
+export async function GET() {
+	const cards: Card[] = await getCards()
+	const response: CardsResponse = { deck: cards }
+	return NextResponse.json(response, { status: 200 })
+}
