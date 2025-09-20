2b26030a94a68ad63a70225498355b5b613d2e50
Adolfo Gante <adolfogante@gmail.com>
2025-09-20 13:47:22 -0700

    feat(api): DELETE handler resets database

--- app/api/cards/route.ts
+++ app/api/cards/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { createCard, getCards } from "@/lib/db";
+import { createCard, deleteCards, getCards } from "@/lib/db";
 import type { Card, CreateCardRequest, CardsResponse, CardResponse } from "@/types/card";
 
 export async function POST() {
@@ -13,4 +13,9 @@ export async function GET() {
 	const cards: Card[] = await getCards()
 	const response: CardsResponse = { deck: cards }
 	return NextResponse.json(response, { status: 200 })
+}
+
+export async function DELETE() {
+	const rowsDeleted = await deleteCards()
+	return NextResponse.json(rowsDeleted, { status: 200 })
 }
