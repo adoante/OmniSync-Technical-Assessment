c52f4e37724056767d3e48d77edaa4c9d9c4a5b8
Adolfo Gante <adolfogante@gmail.com>
2025-09-19 19:22:00 -0700

    feat(card): add optional prop createdAt and Card is not a button

--- components/card.tsx
+++ components/card.tsx
@@ -1,19 +1,16 @@
 interface CardProps {
 	id: number
 	clicks: number
-	createdAt: Date
+	createdAt?: Date
 }
 
 const CardComponent = ({ id, clicks, createdAt }: CardProps) => {
 	return (
-		<button
-			onClick={() => console.log(`Card: ${id} clicked!`)}
-			className="border md:w-40 md:px-4 md:py-6 md:space-y-6 space-y-4 w-30 py-3"
-		>
+		<div className="border text-center md:w-40 md:px-4 md:py-6 md:space-y-6 space-y-4 w-30 py-3">
 			<p>{clicks}</p>
 			<p>{id}</p>
-			<p className="text-center"> {createdAt?.toLocaleTimeString() ?? "00:00:00 PM"}</p>
-		</button>
+			<p> {createdAt?.toLocaleTimeString() ?? "00:00:00 PM"}</p>
+		</div>
 	)
 }
 
