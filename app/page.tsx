015e421258b275892e4702ffe3c0df34f318683e
Adolfo Gante <adolfogante@gmail.com>
2025-09-19 19:23:53 -0700

    feat(pages): add clear button resets cards
    - changed to work with Card Component changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,60 @@
 "use client"
 
 import { CardComponent } from "@/components/card"
-
+import type { Card } from "@/types/card"
 import { useState, useEffect } from "react"
 
 export default function Home() {
-	const [cards, setCards] = useState<React.ReactNode[]>([])
+	const [cards, setCards] = useState<Card[]>([])
 
 	useEffect(() => {
+		// Dummy data for now
+		const data: Card[] = [
+			{ "id": 1, "clicks": 0 },
+			{ "id": 2, "clicks": 0 },
+			{ "id": 3, "clicks": 0 },
+			{ "id": 4, "clicks": 0 },
+			{ "id": 5, "clicks": 0 },
+			{ "id": 6, "clicks": 0 },
+			{ "id": 7, "clicks": 0 },
+			{ "id": 8, "clicks": 0 },
+		]
 
-		const cardComponents: React.ReactNode[] = []
-		for (let i = 1; i < 9; i++) {
-			cardComponents.push(<CardComponent key={i} id={i} />)
-		}
-
-		setCards(cardComponents)
+		setCards(data)
 	}, [])
 
-	return (
-		<div className="border grid grid-cols-3 grid-rows-x gap-y-4 gap-x-4 md:grid-cols-4 md:grid-rows-2 max-w-fit mx-auto md:gap-y-4 md:gap-x-4">
-			{cards.map((card) => (
-				card
-			))}
+	const incrementClick = (index: number) => {
+		const updatedCards = [...cards]
+		const card = updatedCards[index]
 
-		</div>
+		if (card.clicks == 0) {
+			card.createdAt = new Date()
+		}
+
+		card.clicks = card.clicks += 1
+
+		setCards(updatedCards)
+	}
+
+	const clearCards = () => {
+		const updatedCards = [...cards]
+		updatedCards.forEach((card) => {
+			card.clicks = 0
+			card.createdAt = undefined
+		})
+		setCards(updatedCards)
+	}
+
+	return (
+		<>
+			<button onClick={clearCards}>Clear</button>
+			<div className="border grid grid-cols-3 grid-rows-x gap-y-4 gap-x-4 md:grid-cols-4 md:grid-rows-2 max-w-fit mx-auto md:gap-y-4 md:gap-x-4">
+				{cards.map((card, index) =>
+					<button onClick={() => incrementClick(index)} key={index}>
+						<CardComponent id={card.id} clicks={card.clicks} createdAt={card.createdAt} />
+					</button>
+				)}
+			</div>
+		</>
 	)
 }
