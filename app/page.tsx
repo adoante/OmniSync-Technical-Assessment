8a0133eebb6057307b4edd803d73c221982e8ecc
Adolfo Gante <adolfogante@gmail.com>
2025-09-19 18:03:52 -0700

    feat(pages): add example page rendering Cards - 8 cards in a grid size 2x4 for desktop - grid size 3x3 for mobile

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,28 @@
+"use client"
+
+import { CardComponent } from "@/components/card"
+
+import { useState, useEffect } from "react"
+
 export default function Home() {
+	const [cards, setCards] = useState<React.ReactNode[]>([])
+
+	useEffect(() => {
+
+		const cardComponents: React.ReactNode[] = []
+		for (let i = 1; i < 9; i++) {
+			cardComponents.push(<CardComponent key={i} id={i} />)
+		}
+
+		setCards(cardComponents)
+	}, [])
+
 	return (
-		<>
-		</>
+		<div className="border grid grid-cols-3 grid-rows-x gap-y-4 gap-x-4 md:grid-cols-4 md:grid-rows-2 max-w-fit mx-auto md:gap-y-4 md:gap-x-4">
+			{cards.map((card) => (
+				card
+			))}
+
+		</div>
 	)
 }
