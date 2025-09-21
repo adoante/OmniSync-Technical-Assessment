f1afd4c7ea39d18078ec16ef3582bf8922d15175
Adolfo Gante <adolfogante@gmail.com>
2025-09-20 21:16:18 -0700

    feat(components): updated CardComponent props to match Card type

--- components/card.tsx
+++ components/card.tsx
@@ -1,15 +1,15 @@
 interface CardProps {
 	id: number
 	clicks: number
-	createdAt?: Date
+	time?: Date | null
 }
 
-const CardComponent = ({ id, clicks, createdAt }: CardProps) => {
+const CardComponent = ({ id, clicks, time }: CardProps) => {
 	return (
 		<div className="border text-center md:w-40 md:px-4 md:py-6 md:space-y-6 space-y-4 w-30 py-3">
 			<p>{clicks}</p>
 			<p>{id}</p>
-			<p> {createdAt?.toLocaleTimeString() ?? "00:00:00 PM"}</p>
+			<p> {time?.toLocaleTimeString() ?? "00:00:00 PM"}</p>
 		</div>
 	)
 }
